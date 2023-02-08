6c09a29e7f60f35f8116a93d0914f31c3e66a0ae
VM-Web <mas.usa.gov@gmail.com>
2023-02-07 22:44:56 -0500

    buttons

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { GoBell, GoFileDirectory, GoBookmark, GoFileMedia } from "react-icons/go";
+import Button from './components/button';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    return (
+        <div>
+            <Button primary
+                onClick={() => console.log("handleClick...")}
+                onMouseOver={() => console.log("handleMouseOver...")}
+                className="mb-5"
+            >
+                <GoBell />
+                Click me
+            </Button>
+            <Button secondary ><GoFileDirectory />Click me</Button>
+            <Button success  ><GoBookmark />Click me</Button>
+            <Button warning ><GoFileMedia />Click me</Button>
+            <Button danger >Click me</Button>
+
+            <Button primary rounded ><GoBell />Click me</Button>
+            <Button secondary rounded ><GoFileDirectory />Click me</Button>
+            <Button success rounded ><GoBookmark />Click me</Button>
+            <Button warning rounded ><GoFileMedia />Click me</Button>
+            <Button danger rounded >Click me</Button>
+
+            <Button primary outline >Click me</Button>
+            <Button secondary outline >Click me</Button>
+            <Button success outline >Click me</Button>
+            <Button warning outline >Click me</Button>
+            <Button danger outline >Click me</Button>
+
+            <Button primary outline rounded >Click me</Button>
+            <Button secondary outline rounded >Click me</Button>
+            <Button success outline rounded >Click me</Button>
+            <Button warning outline rounded >Click me</Button>
+            <Button danger outline rounded >Click me</Button>
+        </div >
+    )
 }
 
-export default App;
+export default App
